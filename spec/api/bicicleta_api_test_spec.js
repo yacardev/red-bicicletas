b3492dc2d5d1ba8762a501eb77e4668d274d0e41
yacardev <matias.exe.lujan@gmail.com>
2020-10-06 22:31:10 -0300

    week 2. 2

---
+++ spec/api/bicicleta_api_test_spec.js
@@ -0,0 +1,36 @@
+var Bicicleta = require('../../models/bicicletas');
+var request = require('request');
+var server = require('../../bin/www');
+
+describe('testear la api', () => {
+    describe('GET Bicicletas', () => {
+        it('Status 200', () => {
+            expect(Bicicleta.all.length).toBe(0);
+            var aBici = new Bicicleta(1, 'verde', 'urbana', [-34.6064996, -58.4356329]);
+
+            Bicicleta.add(aBici);
+
+            request.get('http://localhost:3000/api/bicicletas', function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+            });
+        });
+    });
+
+    describe('POST Bicicletas /create', () => {
+        it('Status 200', (done) => {
+            var headers = { 'content-type': 'application/json' };
+            //var aBici = '{"id": 5, "color": "verde", "modelo": "urbana","lat": -34.6064996, "lng":-58.4356329}';
+            var aBici = '{"id": 5, "color": "verde", "modelo": "urbana","lat": -34.6064996, "lng":-58.4356329}';
+
+            request.post({
+                headers: headers,
+                url: 'http://localhost:3000/api/bicicletas/create',
+                body: aBici
+            }, function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                expect(Bicicleta.findById(5).color).toBe("verde");
+                done();
+            });
+        });
+    });
+});
